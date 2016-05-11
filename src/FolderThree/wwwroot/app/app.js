c7f51d595bb8a0343a701728155a885bebcca2c9
Anton Vdovin <antonvdovin92@gmail.com>
2016-05-11 19:11:34 +0300

    recomment

--- src/FolderThree/wwwroot/app/app.js
+++ src/FolderThree/wwwroot/app/app.js
@@ -2,6 +2,7 @@
     'use strict';
     var app = angular.module('app', ['ngRoute']);
 
+    // route config
     app.config(function($routeProvider) {
         $routeProvider
             .otherwise({
